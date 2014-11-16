614c37f7c7d8dd58d57c61b526cb3a83f2443b45
Per Johansson <per@johansson.name>
2014-11-16 19:58:05 +0200

    Fix so loadCountryAndQuotes is loaded after storage

--- background.js
+++ background.js
@@ -162,8 +162,7 @@ const DirectCurrencyConverter = (function() {
                         controller.loadQuotes();
                     }
                 };
-                // If freegeoip won't work
-                // informationHolder.setUserCountry("AX");
+                // If no country was stored, get it again
                 if (informationHolder.convertToCountry === null || informationHolder.convertToCountry == null) {
                     const method = "GET";
                     request.open(method, urlString);
@@ -806,6 +805,7 @@ const DirectCurrencyConverter = (function() {
                         }
                     }
                     chrome.storage.local.set(storage);
+                    controller.loadUserCountryAndQuotes();
                 });
             },
             get convertToCurrency () {
@@ -1410,9 +1410,8 @@ const DirectCurrencyConverter = (function() {
         anEventAggregator.subscribe("resetSettings", function() {
             informationHolder.resetSettings();
             // tabsInterface.getSettingsTab().close();
-            storageService.init(informationHolder.getDefaultEnabled());
             controller.loadStorage();
-            controller.loadUserCountryAndQuotes();
+            // controller.loadUserCountryAndQuotes();
         });
         anEventAggregator.subscribe("tabActivated", function(eventArgs) {
             const customTabObject = tabsInterface.getCustomTabObjects()[eventArgs.tab.id];
@@ -1451,7 +1450,6 @@ const DirectCurrencyConverter = (function() {
     const tabsInterface = new TabsInterface(urlProvider, informationHolder);
     const controller = new Controller(eventAggregator);
     controller.loadStorage();
-    controller.loadUserCountryAndQuotes();
     tabsInterface.registerToTabsEvents();
     var buttonStatus = informationHolder.conversionEnabled;
     const onBrowserAction = function() {
