cffe5add41dc9c165f82624ab774c86529a5e4ba
Per Johansson <per@johansson.name>
2015-05-07 23:23:07 +0300

    Register to tabs events. Only work on "http*" pages.

--- gc-contentInterface.js
+++ gc-contentInterface.js
@@ -8,6 +8,7 @@ const GcContentInterface = function(anInformationHolder) {
     "use strict";
     let settingsWorker = null;
     let testPageWorker = null;
+    var isRegisteredToTabsEvents = false;
     const customTabObjects = [];
     const CustomTabObject = function() {
         "use strict";
@@ -20,7 +21,7 @@ const GcContentInterface = function(anInformationHolder) {
             customTabObject.port.postMessage(status);
         }
     };
-    const attachHandler = function(tabId, changeInfo) {
+    const attachHandler = function(tabId, changeInfo, tab) {
         var contentPort;
         const finishedTabProcessingHandler = function (aHasConvertedElements) {
             try {
@@ -48,8 +49,8 @@ const GcContentInterface = function(anInformationHolder) {
         };
         console.log("attachHandler tabId " + tabId);
         console.log("attachHandler changeInfo.status " + changeInfo.status);
-        if (changeInfo.status === "complete") {
-            console.log("aWorker.port.emit updateSettings");
+        console.log("attachHandler changeInfo.url " + changeInfo.url);
+        if (changeInfo.status === "complete" && tab && tab.url && tab.url.indexOf("http") === 0) {
             chrome.tabs.executeScript({file: "common/dcc-regexes.js", allFrames: true}, function(){
                 chrome.tabs.executeScript({file: "common/dcc-content.js", allFrames: true}, function(){
                     chrome.tabs.executeScript({file: "dcc-chrome-content-adapter.js", allFrames: true},
@@ -137,80 +138,34 @@ const GcContentInterface = function(anInformationHolder) {
         }
     };
     const registerToTabsEvents = function() {
-        const setTab = function(aTab) {
-            if (aTab.url === "resource://dcc-at-joint-dot-ax/direct-currency-converter-2/data/settings.html") {
-                settingsWorker = aTab.attach({contentScriptFile: ["./common/jquery-2.1.3.min.js", "./common/jquery-ui-1.11.2.min.js", "./common/dcc-settings.js", "./dcc-firefox-settings-adapter.js"]});
-                console.log("settingsWorker.port.emit showSettings");
-                settingsWorker.port.emit("showSettings", new ContentScriptParams(aTab, anInformationHolder));
-                settingsWorker.port.on("saveSettings", function(aContentScriptParams) {
-                    eventAggregator.publish("saveSettings", {
-                        contentScriptParams: aContentScriptParams
-                    });
-                });
-                settingsWorker.port.on("resetSettings", function() {
-                    eventAggregator.publish("resetSettings");
-                });
-                settingsWorker.settingsTab = aTab;
-            }
-            else if (aTab.url === "resource://dcc-at-joint-dot-ax/direct-currency-converter-2/data/common/prices.html") {
-                testPageWorker = aTab.attach({contentScriptFile: ["./common/dcc-regexes.js", "./common/dcc-content.js", "./dcc-firefox-content-adapter.js"]});
-                const finishedTabProcessingHandler = function(aHasConvertedElements) {
-                    try {
-                        if (testPageWorker.tab.customTabObject == null) {
-                            testPageWorker.tab.customTabObject = new CustomTabObject();
-                        }
-                        testPageWorker.tab.customTabObject.isEnabled = anInformationHolder.conversionEnabled;
-                        testPageWorker.tab.customTabObject.workers.push(testPageWorker);
-                        testPageWorker.tab.customTabObject.hasConvertedElements = aHasConvertedElements;
-                    }
-                    catch(err) {
-                        console.error("registerToTabsEvents: " + err);
-                    }
-                };
-                testPageWorker.port.emit("updateSettings", new ContentScriptParams(testPageWorker.tab, anInformationHolder));
-                testPageWorker.port.on("finishedTabProcessing", finishedTabProcessingHandler);
-                testPageWorker.testPageTab = aTab;
-            }
-            else if (aTab.customTabObject == null) {
-                aTab.customTabObject = new CustomTabObject();
-                aTab.customTabObject.isEnabled = anInformationHolder.conversionEnabled;
-                // To set toggle button for this tab
+        const setTabs = function(aTab) {
+            if (customTabObjects[aTab.id] == null) {
+                customTabObjects[aTab.id] = new CustomTabObject();
+                customTabObjects[aTab.id].isEnabled = anInformationHolder.conversionEnabled;
                 eventAggregator.publish("toggleConversion", anInformationHolder.conversionEnabled);
             }
-            if (aTab.customTabObject != null) {
-                let i = aTab.customTabObject.workers.length;
-                while (i--) {
-                    const worker = aTab.customTabObject.workers[i];
-                    if (worker.tab == null) {
-                        aTab.customTabObject.workers.splice(i, 1);
-                    }
-                }
-            }
         };
-        const releaseTab = function(aTab) {
-            if (settingsWorker && settingsWorker.settingsTab) {
+        const releaseTabs = function(aTab) {
+            if (settingsWorker != null && settingsWorker.settingsTab != null) {
                 if (settingsWorker.settingsTab.title == aTab.title) {
                     settingsWorker.settingsTab = null;
                 }
                 else {
-                    aTab.customTabObject = null;
+                    customTabObjects[aTab.id] = null;
                 }
             }
             else {
-                aTab.customTabObject = null;
+                customTabObjects[aTab.id] = null;
             }
         };
-        const activateTab = function() {
-            if (tabs.activeTab.customTabObject) {
-                tabs.activeTab.customTabObject.isEnabled = anInformationHolder.conversionEnabled;
-            }
-            eventAggregator.publish("tabActivated", {
-                tab: tabs.activeTab
+        if (!isRegisteredToTabsEvents) {
+            chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+                if (tab.url.indexOf("http") === 0 && changeInfo.status === "complete") {
+                    setTabs(tab);
+                }
             });
-        };
-        //tabs.on("activate", activateTab);
-        //tabs.on("ready", setTab);
-        //tabs.on("close", releaseTab);
+            isRegisteredToTabsEvents = true;
+        }
     };
     const closeSettingsTab = function() {
         if (settingsWorker && settingsWorker.settingsTab) {
