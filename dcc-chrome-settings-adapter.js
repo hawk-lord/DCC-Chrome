e6266a7c85c793736372fb849c2d91fb8324edbb
Per Johansson <per@johansson.name>
2015-08-14 23:05:24 +0300

    Updated headers.

--- dcc-chrome-settings-adapter.js
+++ dcc-chrome-settings-adapter.js
@@ -1,10 +1,10 @@
 /*
- * © 2014 Per Johansson
+ * © 2014-2015 Per Johansson
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
  * Based on code from Simple Currency Converter
- * https://addons.mozilla.org/en-US/firefox/addon/simple-currency-converter/
+ * https://addons.mozilla.org/addon/simple-currency-converter/
  *
  * Module pattern is used.
  */
