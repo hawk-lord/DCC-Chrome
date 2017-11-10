e7c27f8ea7ec84552d3ed28baacf9f1eeddff1f9
Per Johansson <per@johansson.name>
2017-11-10 21:00:03 +0200

    Changed datasource.

---
+++ gc-yahoo-2-quotes-service.js
@@ -0,0 +1,39 @@
+/*
+ * © Per Johansson
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+"use strict";
+
+const GcYahoo2QuotesServiceProvider = function() {
+    const onComplete = (aResponse) => {
+        try {
+            // console.log("onComplete aResponse " + aResponse);
+            eventAggregator.publish("quotes", aResponse);
+        }
+        catch(err) {
+            console.error("err " + err);
+        }
+    };
+    const fetchQuotes = (aUrlString) => {
+        // console.log("fetchQuotes ");
+        const urlString = aUrlString;
+        const request = new XMLHttpRequest();
+        request.open("GET", aUrlString, true);
+        request.onreadystatechange = () => {
+            if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+                onComplete(request.responseText);
+            }
+        };
+        request.send(null);
+    };
+    return {
+        fetchQuotes: fetchQuotes
+    };
+};
+
+if (typeof exports === "object") {
+    exports.GcYahooQuotesServiceProvider = GcYahooQuotesServiceProvider;
+}
